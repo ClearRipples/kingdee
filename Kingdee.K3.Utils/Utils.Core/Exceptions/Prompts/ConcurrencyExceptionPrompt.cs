caad59e03e949397ae5957c0171febe84383dc57
ClearRipples <liang.hongbo@yahoo.com>
2015-09-30 17:07:10 +0800

    编写日志以及错误异常处理公共类

---
+++ Kingdee.K3.Utils/Utils.Core/Exceptions/Prompts/ConcurrencyExceptionPrompt.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Utils.Core;
+using Utils.Core.Exceptions;
+
+namespace Util.Exceptions.Prompts {
+    /// <summary>
+    /// 并发异常提示
+    /// </summary>
+    public class ConcurrencyExceptionPrompt : IExceptionPrompt{
+        /// <summary>
+        /// 获取异常提示
+        /// </summary>
+        /// <param name="exception">异常</param>
+        public string GetPrompt( Exception exception ) {
+            if ( exception is ConcurrencyException )
+                return R.ConcurrencyExceptionMessage;
+            return string.Empty;
+        }
+    }
+}
